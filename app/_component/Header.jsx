5c1bb4ef51ac46243dcddc06057c981e720ca09a
radi8og <hrishimaz.1235@gmail.com>
2025-04-13 15:57:20 +0530

    Final changes

--- app/_component/Header.jsx
+++ app/_component/Header.jsx
@@ -1,30 +1,85 @@
-import { Button } from '@/components/ui/button'
-import { LayoutGrid, Search, ShoppingBag } from 'lucide-react'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+"use client";
+import { Button } from "@/components/ui/button";
+import { LayoutGrid, Search, ShoppingBag } from "lucide-react";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import { checkAuthState } from "../FirebaseAPI";
+import { getAuth, signOut } from "firebase/auth";
+import Link from "next/link";
 
 function Header() {
+  const [user, setUser] = useState("");
+
+  useEffect(() => {
+    const unsubscribe = checkAuthState((user) => {
+      if (user) {
+        setUser(user);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const handleLogout = async () => {
+    try {
+      const auth = getAuth();
+      await signOut(auth);
+      setUser("");
+      alert("You have successfully logged out!");
+    } catch (error) {
+      console.error("Error during logout:", error);
+      alert("There was an issue logging out.");
+    }
+  };
+  
   return (
-    <div className='h-[80px] p-5  justify-between shadow-lg flex items-center'>
-      <div className='flex items-center gap-20 cursor-pointer'>
-        <Image src={'/logo.png'} alt='logo' width={170} height={50} />
-        <Link href={"#"} className='flex gap-3 items-center cursor-pointer border px-10 py-2 rounded-2xl bg-slate-300'>
-        <LayoutGrid className='h-5 w-5' />
-        Category</Link>
-        <div className='flex justify-center border rounded-2xl px-10 py-2 gap-3'>
+    <div className="p-5 shadow-md h-[80px] justify-between items-center flex">
+      <div className="flex items-center  gap-8">
+        <Image src={"/logo.png"} alt="logo" width={170} height={50} />
+        <Link
+          href={"/category/Fruits"}
+          className="md:flex hidden gap-2 items-center cursor-pointer border rounded-full p-2 px-10 bg-slate-200"
+        >
+          <LayoutGrid className="h-5 w-5" /> Category
+        </Link>
+
+        <div
+          className="md:flex gap-3 items-center border rounded-full p-2
+          px-5 hidden"
+        >
           <Search />
-            <input type='text' placeholder='Search' className='outline-none'/>
+          <input type="text" placeholder="Search" className="outline-none" />
         </div>
       </div>
-      <div className='flex items-center gap-5'>
-        <Link href={"/checkout"} className='flex border text-md px-10 py-2 rounded-2xl'>
-        <ShoppingBag />
-        Checkout</Link>
-        <Link href={"/login"} className='border px-8 py-2 rounded-lg font-bold bg-green-500 hover:bg-yellow-500 :*:'>Login</Link>
+      <div className="flex gap-5 items-center">
+        <Link
+          href={"/checkout"}
+          className="flex outline hover:bg-yellow-200 hover:text-black hover:scale-105 transition-all ease-in-out px-10 rounded-lg py-2 gap-2 items-center text-md font-bold"
+        >
+          <ShoppingBag /> Checkout
+        </Link>
+
+        {!user ? (
+          <Link
+            href={"/login"}
+            className={
+              "cursor-pointer outline hover:bg-green-200 hover:text-black hover:scale-105 transition-all ease-in-out px-4 rounded-lg py-2 gap-2 items-center text-md font-semibold"
+            }
+          >
+            Login
+          </Link>
+        ) : (
+          <Button
+            onClick={handleLogout}
+            variant={"destructive"}
+            className={"cursor-pointer"}
+          >
+            Logout
+          </Button>
+        )}
       </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
